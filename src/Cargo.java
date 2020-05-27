d6b76e50997d31050717e0848362436db24597f1
Bruce <xlii0181@student.monash.edu>
2020-05-27 21:46:18 +0800

    Cargotest

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Cargo {
+    private int cargo_Id;
+    private String cargo;
+    private int quantityAvailable;
+
+    public Cargo(int cargo_Id, String cargo, int quantityAvailable) {
+        this.cargo_Id = cargo_Id;
+        this.cargo = cargo;
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public Cargo() {
+    }
+
+    public int getCargo_Id() {
+        return cargo_Id;
+    }
+
+    public void setCargo_Id(int cargo_Id) {
+        this.cargo_Id = cargo_Id;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
 }
--- src/CargoRequirement.java
+++ src/CargoRequirement.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CargoRequirement {
+    private ArrayList<String> cargoFor;
+    private ArrayList<Cargo> cargoRequired;
+    private int cargoQuantity;
+    private ArrayList<Cargo> cargoList;
+
+    public CargoRequirement() {
+    }
+
+    public CargoRequirement(ArrayList<String> cargoFor, ArrayList<Cargo> cargoRequired, int cargoQuantity, ArrayList<Cargo> cargoList) {
+        this.cargoFor = cargoFor;
+        this.cargoRequired = cargoRequired;
+        this.cargoQuantity = cargoQuantity;
+        this.cargoList = cargoList;
+    }
+
+    public ArrayList<String> getCargoFor() {
+        return cargoFor;
+    }
+
+    public void setCargoFor(ArrayList<String> cargoFor) {
+        this.cargoFor = cargoFor;
+    }
+
+    public ArrayList<Cargo> getCargoRequired() {
+        return cargoRequired;
+    }
+
+    public void setCargoRequired(ArrayList<Cargo> cargoRequired) {
+        this.cargoRequired = cargoRequired;
+    }
+
+    public int getCargoQuantity() {
+        return cargoQuantity;
+    }
+
+    public void setCargoQuantity(int cargoQuantity) {
+        this.cargoQuantity = cargoQuantity;
+    }
+
+    public ArrayList<Cargo> getCargoList() {
+        return cargoList;
+    }
+
+    public void setCargoList(ArrayList<Cargo> cargoList) {
+        this.cargoList = cargoList;
+    }
 }
