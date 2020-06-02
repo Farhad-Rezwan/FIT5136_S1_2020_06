84ea582caee0758373b0ec6bb8be3bd65521b107
Louis <yluu0042@student.monash.edu>
2020-06-03 00:54:47 +1000

    shuttle class

--- src/main/java/missontomars/model/Shuttle/Shuttle.java
+++ src/main/java/missontomars/model/Shuttle/Shuttle.java
@@ -1,6 +1,88 @@
 package main.java.missontomars.model.Shuttle;
 
 public class Shuttle {
-    public Shuttle() {
+    private int id;
+    private String shuttleName;
+    private int manufacturingYear;
+    private int fuelCapacity;
+    private int passengerCapacity;
+    private int cargoCapacity;
+    private int travelSpeed;
+    private String origin;
+
+    public Shuttle(int id, String shuttleName, int manufacturingYear, int fuelCapacity, int passengerCapacity,
+                   int cargoCapacity, int travelSpeed, String origin) {
+        this.id = id;
+        this.shuttleName = shuttleName;
+        this.manufacturingYear = manufacturingYear;
+        this.fuelCapacity = fuelCapacity;
+        this.passengerCapacity = passengerCapacity;
+        this.cargoCapacity = cargoCapacity;
+        this.travelSpeed = travelSpeed;
+        this.origin = origin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShuttleName() {
+        return shuttleName;
+    }
+
+    public void setShuttleName(String shuttleName) {
+        this.shuttleName = shuttleName;
+    }
+
+    public int getManufacturingYear() {
+        return manufacturingYear;
+    }
+
+    public void setManufacturingYear(int manufacturingYear) {
+        this.manufacturingYear = manufacturingYear;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getTravelSpeed() {
+        return travelSpeed;
+    }
+
+    public void setTravelSpeed(int travelSpeed) {
+        this.travelSpeed = travelSpeed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
     }
 }
