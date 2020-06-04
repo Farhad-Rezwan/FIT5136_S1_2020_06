37fa07ea57351c8a7586522939d39e70b379c888
Farhad-Rezwan <frez0003@student.monash.edu>
2020-06-04 11:40:49 +1000

    Adding feature 1 more functionality

--- src/main/java/missontomars/model/Feature2/GetShuttle.java
+++ src/main/java/missontomars/model/Feature2/GetShuttle.java
@@ -22,4 +22,23 @@ public class GetShuttle {
             e.printStackTrace();
         }
     }
+
+    public void readEmployee(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("shuttle.csv"));//换成你的文件名
+            reader.readLine();//第一行信息，为标题信息，不用,如果需要，注释掉
+            String line;
+            while((line=reader.readLine())!=null){
+                String[] item = line.split(",");
+                StringBuilder line1 = new StringBuilder();
+                for (String column : item) {
+                    line1.append(column).append("      ");
+                }
+                //String last = item[item.length-1];
+                System.out.println(line1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
